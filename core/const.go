8a3d604d4921680c8c67645430dca2d2e7a2a15f
Casian Lacatusu <casian.lacatusu@gmail.com>
2020-09-30 15:36:44 +0300

    Send syncing state to server

--- core/const.go
+++ core/const.go
@@ -6,3 +6,4 @@ const TokenFile = "token.dat"
 const HeartbeatInterval = 15 * time.Second
 const PeersInterval = 15 * time.Second
 const MpoolSizeInterval = 15 * time.Second
+const SyncingInterval = 15 * time.Second
